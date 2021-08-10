af114449f76a338f92efeac4220af2f140db0d6c
NalyPon <k.narita122@gmail.com>
2021-08-10 20:50:49 +0900

    じゃんけん処理を１つの関数に統合。 あいこが連続した場合の回数表示を追加。

--- janken/janken.js
+++ janken/janken.js
@@ -1,13 +1,11 @@
-/** 手札オブジェクト */
-const hands = {
-    rock: 0,
-    scissors: 1,
-    paper: 2
-}
-
-const handsLength = Object.keys(hands).length;
-
 /** 定数を定義 */
+const hands = [0, 2, 5];
+const handsLength = hands.length;
+
+const rock = 0;
+const scissors = 2;
+const paper = 5;
+
 const $sazaeStandBy = document.getElementById('sazaeStandBy');
 const initSazaeStandBy = $sazaeStandBy.style.display;
 const $sazaeRock = document.getElementById('sazaeRock');
@@ -23,10 +21,23 @@ const $rockButton = document.getElementById('rock');
 const $scissorsButton = document.getElementById('scissors');
 const $paperButton = document.getElementById('paper');
 
+// あいこカウント
+let drawCount = 0;
+
+/** ボタンイベントを定義 */
+$rockButton.addEventListener('click', function () {
+    janken(rock);
+});
+$scissorsButton.addEventListener('click', function () {
+    janken(scissors);
+});
+$paperButton.addEventListener('click', function () {
+    janken(paper);
+});
+
 // サザエの手札を非表示にする
-$sazaeRock.style.display = 'none';
-$sazaeScissors.style.display = 'none';
-$sazaePaper.style.display = 'none';
+resetEnemyHand();
+
 // メッセージを挿入
 $messageArea.innerHTML = 'じゃんけん～？'
 
@@ -38,91 +49,86 @@ function getRandomInt(max) {
 }
 
 /**
- * Rock button event
+ * じぇんけんイベント
+ * @param {number} playerHand プレイヤーの手札 
  */
-function rockEvent() {
+function janken(playerHand) {
     // サザエの手札を取得
     let enemyHand = getEnemyHand();
 
-    // 勝敗判定
-    if (enemyHand == hands.rock) {
-        drawEvent();
+    // あいこの場合
+    if (playerHand == enemyHand) {
+        draw();
     }
-    else if (enemyHand == hands.scissors) {
-        winEvent();
-    } else if (enemyHand == hands.paper) {
-        loseEvent();
-    }
-}
 
-/**
- * Scissors button event
- */
-function scissorsEvent() {
-    // サザエの手札を取得
-    let enemyHand = getEnemyHand();
+    let lower = playerHand < enemyHand;
+    // あいこ以外の場合
+    switch (playerHand + enemyHand) {
+        // グーとチョキ or チョキとパー
+        case (rock + scissors):
+        case (scissors + paper):
+            if (lower) {
+                win();
+            } else {
+                lose();
+            }
+            break;
+        // パーとグー
+        case (paper + rock):
+            if (lower) {
+                lose();
+            } else {
+                win();
+            }
+    }
 
-    // 勝敗判定
-    if (enemyHand == hands.rock) {
-        loseEvent();
-    }
-    else if (enemyHand == hands.scissors) {
-        drawEvent();
-    } else if (enemyHand == hands.paper) {
-        winEvent();
-    }
-}
-
-/**
- * Paper button event
- */
-function paperEvent() {
-    // サザエの手札を取得
-    let enemyHand = getEnemyHand();
-
-    // 勝敗判定
-    if (enemyHand == hands.rock) {
-        winEvent();
-    }
-    else if (enemyHand == hands.scissors) {
-        loseEvent();
-    } else if (enemyHand == hands.paper) {
-        drawEvent();
-    }
 }
 
 /**
  * サザエの手札を決定し、画面処理を行った後、手札の番号を返却する
  */
 function getEnemyHand() {
+
     // サザエの手札を決定する
-    let enemyHand = getRandomInt(handsLength);
+    let enemyHand = hands[getRandomInt(handsLength)];
+
     // サザエ画面リセット
     $sazaeStandBy.style.display = 'none';
-    $sazaeRock.style.display = 'none';
-    $sazaeScissors.style.display = 'none';
-    $sazaePaper.style.display = 'none';
+    resetEnemyHand();
 
     // グー・チョキ・パーそれぞれの画面処理を行う
-    if (enemyHand == hands.rock) {
-        // サザエ(グー)を表示する
-        $sazaeRock.style.display = initSazaeRock;
-    }
-    else if (enemyHand == hands.scissors) {
-        // サザエ(チョキ)を表示する
-        $sazaeScissors.style.display = initSazaeScissors;
-    } else if (enemyHand == hands.paper) {
-        // サザエ(チョキ)を表示する
-        $sazaePaper.style.display = initSazaePaper;
+    switch (enemyHand) {
+        case rock:
+            // サザエ(グー)を表示する
+            $sazaeRock.style.display = initSazaeRock;
+            break;
+        case scissors:
+            // サザエ(チョキ)を表示する
+            $sazaeScissors.style.display = initSazaeScissors;
+            break;
+        case paper:
+            // サザエ(パー)を表示する
+            $sazaePaper.style.display = initSazaePaper;
+            break;
     }
 
     return enemyHand;
 }
 
 /**
+ * あいこイベント
+ */
+function draw() {
+    // あいこカウントをインクリメント
+    drawCount++;
+    // メッセージを挿入
+    $messageArea.innerHTML = 'ポン、あいこで～？(' + drawCount + '回目)'
+}
+
+/**
  * 勝利イベント
  */
-function winEvent() {
+function win() {
     // じゃんけんボタンを非活性にする
     $rockButton.setAttribute('disabled', true);
     $scissorsButton.setAttribute('disabled', true);
@@ -132,21 +138,31 @@ function winEvent() {
 }
 
 /**
- * あいこイベント
- */
-function drawEvent() {
-    // メッセージを挿入
-    $messageArea.innerHTML = 'ポン、あいこで～？'
-}
-
-/**
  * 敗北イベント
  */
-function loseEvent() {
+function lose() {
     // じゃんけんボタンを非活性にする
     $rockButton.setAttribute('disabled', true);
     $scissorsButton.setAttribute('disabled', true);
     $paperButton.setAttribute('disabled', true);
     // メッセージを挿入
     $messageArea.innerHTML = 'ポン、お前の負け。何で負けたか明日までに考えてこい。'
+}
+
+/**
+ * サザエの手札を非表示にする
+ */
+function resetEnemyHand() {
+    $sazaeRock.style.display = 'none';
+    $sazaeScissors.style.display = 'none';
+    $sazaePaper.style.display = 'none';
+}
+
+/** 
+ * ビジーwaitを使う方法
+ */
+function sleep(waitMsec) {
+    let startMsec = new Date();
+    // 指定ミリ秒間だけループさせる（CPUは常にビジー状態）
+    while (new Date() - startMsec < waitMsec);
 }
